7c17d0ab78fb117d05894b56b8353d37437d3217
Hamilton Clark <dhclark2@ncsu.edu>
2016-12-05 02:06:18 -0500

    NOT DONE. just pushing to make my code more accessible to Gavin.

--- app.rb
+++ app.rb
@@ -23,6 +23,27 @@ get "/show_employee" do
   erb :show_employee
 end
 
+get "/edit_employee" do
+  erb :edit_employee
+
+end
+
+# get "/delete_employee" do
+#   name = params["name"]
+#   database = PG.connect(dbname: "tiy-database")
+#   @rows = database.exec("DELETE * FROM employees WHERE name = $1",[name])
+#
+#   redirect to("/employees")
+# end
+
+get "/search" do
+  id params["id"]
+  database = PG.connect(dbname: "tiy-database")
+  @rows = database.exec("SELECT * FROM employees WHERE id = $1",[id])
+
+  erb :sumbit
+end
+
 get "/new_employee" do
   erb :new_employee
 end
@@ -38,6 +59,21 @@ post "/create_employee" do
 
   database = PG.connect(dbname: "tiy-database")
   database.exec("INSERT INTO employees(name, address, phone, salary, position, github, slack) VALUES($1, $2, $3, $4, $5, $6, $7)",[name, address, phone, salary, position, github, slack])
+
+  redirect to("/employees")
+end
+
+post "/update_employee" do
+  name = params["name"]
+  address = params["address"]
+  phone = params["phone"]
+  salary = params["salary"]
+  position = params["position"]
+  github = params["github"]
+  slack = params["slack"]
+
+  database = PG.connect(dbname: "tiy-database")
+  @rows = database.exec("UPDATE employees where id =$1,(name, address, phone, salary, position, github, slack) SET VALUES($1, $2, $3, $4, $5, $6, $7)",[name, address, phone, salary, position, github, slack])
 
   redirect to("/employees")
 end
